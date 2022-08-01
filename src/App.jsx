4ec77838fcde0bec871a361917f88b2777a3de3d
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-01 23:32:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './Pages/Home'
+
+function App() {
+
+
+  return (
+    <div className="App">
+   <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Dark from '../assets/dark.png'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="name">Vishal Chauhan</div>
+        <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Contacts</li>
+            <li><img src={Dark} width="50px" height="50px"/></li>
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
