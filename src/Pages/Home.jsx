1f8f026b21faa809df708f688f9261d266c2ebda
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-02 23:21:55 +0530

    home page elements added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import './Home.css'
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
     <Navbar/>
-    </>
+    <div className="intro">
+<div className="hello">Hello👋</div>
+<div className="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur et earum temporibus impedit corrupti officia. Fugit, quam aspernatur mollitia voluptate est ipsam iusto harum. Libero, hic. Nam ea perspiciatis voluptas aperiam deleniti labore voluptate obcaecati.</div>
+<div className="in-touch">get in touch</div>
+    </div>
+    </div>
   )
 }
 
