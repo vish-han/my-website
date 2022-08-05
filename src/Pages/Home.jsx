aa5e400bfa56ae18628a0fb2354e87648fb4ec84
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-08-05 16:50:48 +0530

    image added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import './Home.css'
-
+import MyPic from '../assets/mypic.png'
 const Home = () => {
   return (
     <div className="home">
     <Navbar/>
 <div className="container">
 <div className="left">
-    <div className="intro">
+      <div className="intro">
 <div className="hello">Hello👋</div>
 <div className="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur et earum temporibus impedit corrupti officia. Fugit, quam aspernatur mollitia voluptate est ipsam iusto harum. Libero, hic. Nam ea perspiciatis voluptas aperiam deleniti labore voluptate obcaecati.</div>
 <div className="in-touch">get in touch</div>
     </div></div>
-<div className="right"></div>
+<div className="right">
+  <img src={MyPic} className="mypic" alt="" srcset=""/>
+</div>
 </div>
     </div>
   )
